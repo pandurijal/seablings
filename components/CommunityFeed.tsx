d38a7594fda68b8e7dfa5af22f5a7f619f7bc626
Pandu Rijal Pasa <pandurijal96@gmail.com>
2026-09-08 00:16:21 +0200

    refactor: lock to fixed 4-column grid (was auto-fit)

--- components/CommunityFeed.tsx
+++ components/CommunityFeed.tsx
@@ -163,7 +163,7 @@ const CommunityFeed: React.FC = () => {
         </a>
       </div>
 
-      <div className="px-2 grid gap-4 grid-cols-1 md:grid-cols-[repeat(auto-fit,minmax(min(100%,280px),1fr))]">
+      <div className="px-2 grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {data.items.map((tweet) => (
           <TweetCard key={tweet.id} tweet={tweet} />
         ))}
